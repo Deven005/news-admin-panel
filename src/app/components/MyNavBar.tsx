44a82a73ff6037acaef1d3ceecd39bfdae6e59f2
devenChavda-dev <deven.chavda@preeshe.com>
2024-08-31 02:10:27 +0530

    Updated UI so it don't overlap the title Admin Panel...

--- src/app/components/MyNavBar.tsx
+++ src/app/components/MyNavBar.tsx
@@ -91,41 +91,75 @@ const MyNavBar = () => {
   }, [pathname]);
 
   return isAuthenticated ? (
-    <nav className="navbar bg-base-100 shadow-md border-b border-gray-200">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
+    <nav className="navbar bg-base-100 shadow-md border-b border-gray-200 z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Navbar Start */}
+        <div className="flex items-center">
+          <div className="dropdown lg:hidden">
+            <label tabIndex={0} className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52 transition-transform transform-gpu"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52 transition-transform transform-gpu"
-          >
+              {userLinks.map((link) => (
+                <li
+                  key={link.href}
+                  className={
+                    activeLink === link.href ? "bg-gray-200 rounded-md" : ""
+                  }
+                >
+                  <Link
+                    href={link.href}
+                    className={
+                      activeLink === link.href
+                        ? "text-blue-600 font-semibold"
+                        : ""
+                    }
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Link className="btn btn-ghost normal-case text-xl" href="/">
+            {isAdmin ? "Admin" : isReporter ? "Reporter" : ""} Panel
+          </Link>
+        </div>
+
+        {/* Navbar Center */}
+        <div className="hidden lg:flex flex-grow justify-center">
+          <ul className="menu menu-horizontal space-x-4">
             {userLinks.map((link) => (
               <li
                 key={link.href}
-                className={activeLink === link.href ? "bg-gray-200" : ""}
+                className={
+                  activeLink === link.href ? "bg-gray-200 rounded-md" : ""
+                }
               >
                 <Link
                   href={link.href}
                   className={
                     activeLink === link.href
                       ? "text-blue-600 font-semibold"
-                      : ""
+                      : "hover:text-blue-600 transition-colors duration-200"
                   }
+                  onClick={() => setActiveLink(link.href)}
                 >
                   {link.label}
                 </Link>
@@ -133,64 +167,39 @@ const MyNavBar = () => {
             ))}
           </ul>
         </div>
-        <Link className="btn btn-ghost normal-case text-xl" href="/">
-          {isAdmin ? "Admin" : isReporter ? "Reporter" : ""} Panel
-        </Link>
-      </div>
 
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {userLinks.map((link) => (
-            <li
-              key={link.href}
-              className={
-                activeLink === link.href ? "bg-gray-200 rounded-md" : ""
-              }
+        {/* Navbar End */}
+        <div className="flex items-center space-x-4">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
             >
-              <Link
-                href={link.href}
-                className={
-                  activeLink === link.href ? "text-blue-600 font-semibold" : ""
-                }
-                onClick={() => setActiveLink(link.href)}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-              <Image
-                alt="User Avatar"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                className="object-cover"
-                height={100}
-                width={100}
-              />
+              <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <Image
+                  alt="User Avatar"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  className="object-cover"
+                  height={100}
+                  width={100}
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52 transition-transform transform-gpu"
+            >
+              <li>
+                <p
+                  onClick={() => logout(null)}
+                  className="cursor-pointer text-red-600 hover:text-red-800 transition-colors duration-200"
+                >
+                  Logout
+                </p>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52 transition-transform transform-gpu"
-          >
-            <li>
-              <p
-                onClick={() => logout(null)}
-                className="cursor-pointer text-red-600 hover:text-red-800 transition-colors duration-200"
-              >
-                Logout
-              </p>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
